5a93c6207175eb9a2592748647d67fa4e8a19e74
unknown <mo_silva1015@icloud.com>
2020-01-30 18:26:29 -0300

    feat: change css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React, { useState } from "react";
-import Header from "./Header";
+import React from "react";
+
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function incrementCounter() {
-    setCounter(counter + 1);
-  }
-
-  function desincrementCounter() {
-    setCounter(counter - 1);
-  }
-
   return (
-    <>
-      <h1>Contador: {counter}</h1>
-      <button onClick={incrementCounter}>Incrementar</button>
-      <button onClick={desincrementCounter}>Desincrementar</button>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Cadastrar</button>
+        </form>
+      </aside>
+      <main></main>
+    </div>
   );
 }
 
